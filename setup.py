106c7fa19a443f6d1c83d03a3ca5c0f59e0daf65
ChickenWithACrown <125937747+ChickenWithACrown@users.noreply.github.com>
2024-06-07 01:33:39 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="5",
+    version="0.1",
+    description="A brief description of your package",
+    author="ChickenWithACrown",
+    author_email="10ethicalhacker10@gmail.com",
+    url="https://github.com/ChickenWithACrown/Forrce-OP",
+    packages=find_packages(),  
+    install_requires=[
+        
+    ],
+    entry_points={
+       
+    },
+    classifiers=[
+        
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
+
